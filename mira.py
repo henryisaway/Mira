bc6ad52657a68a6857c53f0e89cfcbd3c2842adf
henry <152448987+henryisaway@users.noreply.github.com>
2026-07-28 14:16:24 -0300

    Squashed commit of the following:
    commit 753d3c77dfc20877675fd45df57bb8678e219b49
    Author: henry <152448987+henryisaway@users.noreply.github.com>
    Date:   Tue Jul 28 14:15:06 2026 -0300
    
        refactor: reorganise repo
    
        Rids the repository of the stable/ and dev/ directories, since those were redundant artifacts from pre-git development.
    
    commit d8a36a5835af483aef19e3ebee7b946b14b93dbb
    Author: henry <152448987+henryisaway@users.noreply.github.com>
    Date:   Mon Jun 15 13:24:31 2026 -0300
    
        fix: asyncio
    
        updated how the bot handles the main loop to reflect changes in the asyncio library
    
    commit 96c237f55881c22b3c467d8f113ca227fd199921
    Merge: 991e789 1c44ad8
    Author: Henry <henryisaway@gmail.com>
    Date:   Wed Jun 4 22:19:27 2025 -0300
    
        extra: merged updated dice parser
    
    commit 991e789fd6a1982ecdde4d5b2db84de421e8e392
    Author: Henry <henryisaway@gmail.com>
    Date:   Mon Sep 30 19:10:16 2024 -0300
    
        Fixed dotenv procedure

--- mira.py
+++ mira.py
@@ -1,7 +1,6 @@
 # Library Importing
 import discord
 from discord.ext import commands
-import token
 import sqlite3
 import os
 import asyncio
@@ -18,7 +17,7 @@ coloramaINIT()
 # Bot variables setup
 load_dotenv(".env")
 token: str = os.getenv("DISCORD_TOKEN")
-version = "2.1.1"
+version = "2.1.2"
 intents = discord.Intents.all()
 activity = f"Version {version}"
 
@@ -28,8 +27,8 @@ activity = f"Version {version}"
 async def getPrefixMain(bot, message):
     return utils.getPrefix(message)
 
-luna = commands.Bot(command_prefix = getPrefixMain, intents=intents)
-luna.remove_command("help")
+mira = commands.Bot(command_prefix = getPrefixMain, intents=intents)
+mira.remove_command("help")
 #-----------------------------------------------------------
 
 # Prefixes database connection
@@ -47,13 +46,13 @@ async def load():
         if filename.endswith(".py") and filename.startswith("module-"):
             try:
                 print(f"Loading extension: {Fore.RED}{Style.BRIGHT}{filename}...{Style.RESET_ALL}")
-                await luna.load_extension(f"modules.{filename[:-3]}")
+                await mira.load_extension(f"modules.{filename[:-3]}")
             except Exception as e:
                 print(f"{Fore.RED}Failed to load extension {filename}. Error: {e}{Style.RESET_ALL}")
                 await shutdown()
 
 async def shutdown():
-    await luna.close()
+    await mira.close()
 
 async def main():
     try:
@@ -64,37 +63,35 @@ async def main():
             
         await load()
         print(f"{Fore.YELLOW}Logging in...{Style.RESET_ALL}")
-        await luna.start(token)
+        await mira.start(token)
     except KeyboardInterrupt:
-        await luna.close()
-        await luna.on_disconnect()
+        await mira.close()
+        await mira.on_disconnect()
     
-@luna.event
+@mira.event
 async def on_ready():
     try:
         if os.name == 'nt':
             os.system("clr")
         else:
             os.system("clear")
-        print(f"{Fore.GREEN}Logged in{Style.RESET_ALL} as {Fore.BLUE}{Style.BRIGHT}{luna.user.name}{Style.RESET_ALL}")
-        await luna.change_presence(activity=discord.Game(name=activity))
+        print(f"{Fore.GREEN}Logged in{Style.RESET_ALL} as {Fore.BLUE}{Style.BRIGHT}{mira.user.name}{Style.RESET_ALL}")
+        await mira.change_presence(activity=discord.Game(name=activity))
     except Exception as e:
         print(e)
 
-@luna.event
+@mira.event
 async def on_connect():
     print(f"\nSucessfully connected to Discord.")
 
-@luna.event
+@mira.event
 async def on_disconnect():
     conn.close()
     print(f"\nSucessfully disconnected from Discord.")
 #-----------------------------------------------------------
 
 if __name__ == "__main__":
-    loop = asyncio.get_event_loop()
-
     try:
-        loop.run_until_complete(main())
+        asyncio.run(main())
     except KeyboardInterrupt:
-        loop.run_until_complete(shutdown())
+        pass
